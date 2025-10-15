258cf4fd9d64fa4d875747d8ec547a2713286208
Mauricio Almeida <mfa1712@gmail.com>
2025-10-15 16:34:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1370477818-242d2",
+  "appId": "1:99483314003:web:b8ab6f39e0f4b053e4e3a6",
+  "apiKey": "AIzaSyAAFiskRnNVZJMhZC5-ntbWehQ9LCbny1E",
+  "authDomain": "studio-1370477818-242d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "99483314003"
+};
